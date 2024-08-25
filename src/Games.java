774da4a042a022f2b5f4d5c0cba60d3da5e64694
kul-arun <kul.arun21@gmail.com>
2024-08-25 23:19:55 +0200

    Change implementation logic

--- src/Games.java
+++ src/Games.java
@@ -6,19 +6,15 @@ public class Games {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         byte n = scanner.nextByte();
-        byte[] home = new byte[n];
-        byte[] guest = new byte[n];
+        byte[] home = new byte[101];
+        byte[] guest = new byte[101];
         for (byte i=0; i<n; ++i) {
-            home[i] = scanner.nextByte();
-            guest[i] = scanner.nextByte();
+            ++home[scanner.nextByte()];
+            ++guest[scanner.nextByte()];
         }
         int count = 0;
-        for (byte i=0; i<n; ++i) {
-            for (byte j=0; j<n; ++j) {
-                if (home[i] == guest[j]) {
-                    ++count;
-                }
-            }
+        for (byte i=1; i<101; ++i) {
+            count += home[i] * guest[i];
         }
         System.out.println(count);
     }
