0f4374166c9f0d1979d41617113cdf893c05dc78
kul-arun <kul.arun21@gmail.com>
2024-09-18 06:55:50 +0200

    Add Problem 2009A

---
+++ src/Minimize.cpp
@@ -0,0 +1,13 @@
+// 2009A. Minimize!
+
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int a, b;
+        std::cin >> a >> b;
+        std::cout << b-a << '\n';
+    }
+}
