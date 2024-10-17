b49b3fcdfbcf636110be588e286d2f71d2a527d1
kul-arun <kul.arun21@gmail.com>
2024-10-17 14:29:06 +0200

    Change implementation logic

--- src/Square.cpp
+++ src/Square.cpp
@@ -6,22 +6,9 @@ int main() {
     int t;
     std::cin >> t;
     while (t--) {
-        int min_x=1001, max_x=-1001;
-        int min_y=1001, max_y=-1001;
-        for (int x, y, i=0; i<4 && std::cin >> x >> y; ++i) {
-            if (x < min_x) {
-                min_x = x;
-            }
-            if (x > max_x) {
-                max_x = x;
-            }
-            if (y < min_y) {
-                min_y = y;
-            }
-            if (y > max_y) {
-                max_y = y;
-            }
-        }
-        std::cout << (max_x-min_x) * (max_y-min_y) << '\n';
+        int x1, y1, x2, y2;
+        std::cin >> x1 >> y1 >> x2 >> y2;
+        std::cin >> x1 >> y1 >> x2 >> y2;
+        std::cout << (x1==x2 ? (y1-y2)*(y1-y2) : (x1-x2)*(x1-x2)) << '\n';
     }
 }
