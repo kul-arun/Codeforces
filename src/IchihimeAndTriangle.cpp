d8f8caa4f217da50a7b16b1a0936ad60494276d9
kul-arun <kul.arun21@gmail.com>
2024-10-21 21:45:49 +0200

    Add Problem 1337A

---
+++ src/IchihimeAndTriangle.cpp
@@ -0,0 +1,14 @@
+// 1337A. Ichihime and Triangle
+
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int a, b, c, d;
+        std::cin >> a >> b >> c >> d;
+        std::cout << b << ' ' << c << ' ' << c << '\n';
+    }
+}
+
