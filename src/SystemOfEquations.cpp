afcbacf37158cef89137ba90caa568bedae7b433
kul-arun <kul.arun21@gmail.com>
2024-10-21 21:02:31 +0200

    Add Problem 214A

---
+++ src/SystemOfEquations.cpp
@@ -0,0 +1,16 @@
+// 214A. System of Equations
+
+#include <iostream>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    int count = 0;
+    for (int a=0; a*a<=n; ++a) {
+        int b = n-a*a;
+        if (a+b*b == m) {
+            ++count;
+        }
+    }
+    std::cout << count << '\n';
+}
